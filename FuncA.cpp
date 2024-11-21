270e266618fe79b97208be0072e7d095223ff378
gityevheniia <bukhalo.yevheniia@lll.kpi.ua>
2024-11-21 17:52:15 +0200

    Finished FuncA implementation with comments

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,5 +1,13 @@
 #include "FuncA.h"
-
-double FuncA::calculate() {
-    return 0; // Базова реалізація повертає 0
+#include <cmath>
+// Клас FuncA реалізує обчислення значення ln(1+x) за допомогою ряду
+// Параметри:
+// - n: кількість елементів ряду
+// - x: значення аргументу
+double FuncA::calculate(int n, double x) {
+    double sum = 0.0;
+    for (int i = 1; i <= n; i++) {
+        sum += pow(-1, i-1) * pow(x, i) / i;
+    }
+    return sum;
 }

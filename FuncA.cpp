c424c8b5dbf49831de8e4d3c2918fc2d66e570b9
gityevheniia <bukhalo.yevheniia@lll.kpi.ua>
2024-11-21 17:27:20 +0200

    Initial implementation of FuncA

---
+++ FuncA.cpp
@@ -0,0 +1,5 @@
+#include "FuncA.h"
+
+double FuncA::calculate() {
+    return 0; // Базова реалізація повертає 0
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "FuncA.h"
+
+int main() {
+    FuncA func;
+    std::cout << "Result: " << func.calculate() << std::endl;
+    return 0;
+}
